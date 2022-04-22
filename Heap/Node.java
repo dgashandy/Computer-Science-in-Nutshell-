37e29a062028e507d3dc425f90ea16bc4cf9ee6a
dgashandy <99483955+dgashandy@users.noreply.github.com>
2022-04-22 20:16:37 +0700

    Create Node.java

---
+++ Heap/Node.java
@@ -0,0 +1,15 @@
+package latihan83;
+
+public class Node {
+	private int idata;
+	
+	public Node(int key){
+		idata = key;
+	}
+	public int getKey(){
+		return idata;
+	}
+	public void setKey(int id){
+		idata = id;
+	}
+}
